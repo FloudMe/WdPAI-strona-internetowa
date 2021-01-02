ad421bd78d503f74440c8437ab48c5377850fef3
Damian Marecik <marecik1999@gmail.com>
2021-01-02 12:54:33 +0100

    Routing php

---
+++ Routing.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'src/controllers/DeafultController.php';
+
+class Routing {
+
+  public static $routes;
+
+  public static function get($url, $view) {
+    self::$routes[$url] = $view;
+  }
+
+  public static function run ($url) {
+    $action = explode("/", $url)[0];
+    if (!array_key_exists($action, self::$routes)) {
+      die("Wrong url!");
+    }
+
+    $controller = self::$routes[$action];
+    // die($controller);
+    $object = new $controller;
+    $action = $action ?: 'index';
+
+    $object->$action();
+  }
+}
